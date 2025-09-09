0bb1aac30bdf3a86d3e435f94850c945d1e7d710
Vahe Ghazaryan <vaheghazaryan122@gmail.com>
2025-09-10 00:40:51 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,6 @@ from PySide6.QtWidgets import QApplication
 import qasync
 from gui.main_GUI import ExpenseTracker
 
-# from gui2.main_window import MainWindow
-
-# app = QApplication(sys.argv)
-# window = MainWindow()
-# window.show()
-# sys.exit(app.exec())
-
-
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = ExpenseTracker()
@@ -24,4 +15,5 @@ if __name__ == "__main__":
     asyncio.set_event_loop(loop)
 
     with loop:
+
         loop.run_forever()
